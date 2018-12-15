6a251717d4d1791f2373c9e882e0bb3736157696
XELF <xelfia@gmail.com>
2018-12-15 23:14:06 +0900

    Update SPCRJointDynamicsCollider.cs

--- unity/Assets/Scripts/SPARKCREATIVE/Dynamics/SPCRJointDynamicsCollider.cs
+++ unity/Assets/Scripts/SPARKCREATIVE/Dynamics/SPCRJointDynamicsCollider.cs
@@ -11,78 +11,65 @@
 
 using UnityEngine;
 
-public class SPCRJointDynamicsCollider : MonoBehaviour
-{
-    Transform _RefTransform;
+public class SPCRJointDynamicsCollider : MonoBehaviour {
+	[SerializeField, Range(0.0f, 5.0f)]
+	private float _Radius = 0.05f;
+	[SerializeField, Range(0.0f, 5.0f)]
+	private float _Height = 0.0f;
+	[SerializeField, Range(0.0f, 1.0f)]
+	private float _Friction = 0.5f;
 
-    [SerializeField]
-    [Range(0.0f, 5.0f)]
-    float _Radius = 0.05f;
-    [SerializeField]
-    [Range(0.0f, 5.0f)]
-    float _Height = 0.0f;
-    [SerializeField]
-    [Range(0.0f, 1.0f)]
-    float _Friction = 0.5f;
+	public Transform RefTransform { get; private set; }
+	public float Radius { get { return _Radius; } }
+	public float Height { get { return _Height; } }
+	public float Friction { get { return _Friction; } }
 
-    public Transform RefTransform { get { return _RefTransform; } }
-    public float Radius { get { return _Radius; } }
-    public float Height { get { return _Height; } }
-    public float Friction { get { return _Friction; } }
+	public bool IsCapsule { get { return _Height > 0.0f; } }
 
-    public bool IsCapsule { get { return _Height > 0.0f; } }
+	private void Awake() {
+		RefTransform = transform;
+	}
 
-    void Awake()
-    {
-        _RefTransform = transform;
-    }
+	private void OnDrawGizmos() {
+		Gizmos.color = Color.gray;
+		if (IsCapsule) {
+			var halfLength = _Height / 2;
+			var up = Vector3.up * halfLength;
+			var down = Vector3.down * halfLength;
+			var right = Vector3.right * _Radius;
+			var forward = Vector3.forward * _Radius;
 
-    void OnDrawGizmos()
-    {
-        Gizmos.color = Color.gray;
-        if (IsCapsule)
-        {
-            var halfLength = _Height / 2;
-            var up = Vector3.up * halfLength;
-            var down = Vector3.down * halfLength;
-            var right = Vector3.right * _Radius;
-            var forward = Vector3.forward * _Radius;
+			var mOld = Gizmos.matrix;
 
-            var mOld = Gizmos.matrix;
+			Gizmos.matrix = Matrix4x4.TRS(transform.position, transform.rotation, Vector3.one);
+			Gizmos.DrawLine(right - up, right + up);
+			Gizmos.DrawLine(-right - up, -right + up);
+			Gizmos.DrawLine(forward - up, forward + up);
+			Gizmos.DrawLine(-forward - up, -forward + up);
 
-            Gizmos.matrix = Matrix4x4.TRS(transform.position, transform.rotation, Vector3.one);
-            Gizmos.DrawLine(right - up, right + up);
-            Gizmos.DrawLine(-right - up, -right + up);
-            Gizmos.DrawLine(forward - up, forward + up);
-            Gizmos.DrawLine(-forward - up, -forward + up);
+			Gizmos.matrix = Matrix4x4.Translate(transform.position + transform.rotation * up) * Matrix4x4.Rotate(transform.rotation * Quaternion.AngleAxis(90, Vector3.forward));
+			DrawWireArc(_Radius, 180);
+			Gizmos.matrix = Matrix4x4.Translate(transform.position + transform.rotation * up) * Matrix4x4.Rotate(transform.rotation * Quaternion.AngleAxis(90, Vector3.up) * Quaternion.AngleAxis(90, Vector3.forward));
+			DrawWireArc(_Radius, 180);
+			Gizmos.matrix = Matrix4x4.Translate(transform.position + transform.rotation * down) * Matrix4x4.Rotate(transform.rotation * Quaternion.AngleAxis(90, Vector3.up) * Quaternion.AngleAxis(-90, Vector3.forward));
+			DrawWireArc(_Radius, 180);
+			Gizmos.matrix = Matrix4x4.Translate(transform.position + transform.rotation * down) * Matrix4x4.Rotate(transform.rotation * Quaternion.AngleAxis(-90, Vector3.forward));
+			DrawWireArc(_Radius, 180);
 
-            Gizmos.matrix = Matrix4x4.Translate(transform.position + transform.rotation * up) * Matrix4x4.Rotate(transform.rotation * Quaternion.AngleAxis(90, Vector3.forward));
-            DrawWireArc(_Radius, 180);
-            Gizmos.matrix = Matrix4x4.Translate(transform.position + transform.rotation * up) * Matrix4x4.Rotate(transform.rotation * Quaternion.AngleAxis(90, Vector3.up) * Quaternion.AngleAxis(90, Vector3.forward));
-            DrawWireArc(_Radius, 180);
-            Gizmos.matrix = Matrix4x4.Translate(transform.position + transform.rotation * down) * Matrix4x4.Rotate(transform.rotation * Quaternion.AngleAxis(90, Vector3.up) * Quaternion.AngleAxis(-90, Vector3.forward));
-            DrawWireArc(_Radius, 180);
-            Gizmos.matrix = Matrix4x4.Translate(transform.position + transform.rotation * down) * Matrix4x4.Rotate(transform.rotation * Quaternion.AngleAxis(-90, Vector3.forward));
-            DrawWireArc(_Radius, 180);
+			Gizmos.matrix = mOld;
+		} else {
+			Gizmos.DrawWireSphere(transform.position, _Radius);
+		}
+	}
 
-            Gizmos.matrix = mOld;
-        }
-        else
-        {
-            Gizmos.DrawWireSphere(transform.position, _Radius);
-        }
-    }
-
-    void DrawWireArc(float radius, float angle)
-    {
-        Vector3 from = Vector3.forward * radius;
-        var step = Mathf.RoundToInt(angle / 15.0f);
-        for (int i = 0; i <= angle; i += step)
-        {
-            var rad = (float)i * Mathf.Deg2Rad;
-            var to = new Vector3(radius * Mathf.Sin(rad), 0, radius * Mathf.Cos(rad));
-            Gizmos.DrawLine(from, to);
-            from = to;
-        }
-    }
+	private static void DrawWireArc(float radius, float angle) {
+		var from = Vector3.forward * radius;
+		var step = Mathf.RoundToInt(angle / 15.0f);
+		for (int i = 0; i <= angle; i += step) {
+			var rad = i * Mathf.Deg2Rad;
+			var to = new Vector3(radius * Mathf.Sin(rad), 0, radius * Mathf.Cos(rad));
+			Gizmos.DrawLine(from, to);
+			from = to;
+		}
+	}
 }
